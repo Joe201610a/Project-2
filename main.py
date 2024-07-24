bfe3cb2758750e298c741862487a7889e9e83d0d
Patrick Hani <65254472+patrick856@users.noreply.github.com>
2024-07-24 19:44:09 +0300

    Update main.py
    base code

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
+import time
 
+print("Once upon a time...")
+time.sleep(1)
+print("Two friends watching the news when suddenly...")
+time.sleep(0.5)
+print("BRREAKING NEWS! SCIENTESTS HAVE DISCOVERED AN ASTEROID AS BIG AS YO MAMA")
+time.sleep(0.2)
+print("AND ITS COMING TOWARDS EARTH AT INCREASING SPEEDS OF VERY FAST SPEEDS FAST")
