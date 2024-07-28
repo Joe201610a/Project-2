cd3f0272c046e7c608752d44500db9b71793f95a
Joe201610a <67336496+Joe201610a@users.noreply.github.com>
2024-07-28 08:36:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 import time
 import random
 import sys
@@ -7,25 +6,30 @@ totalscore = 0
 backpack = []
 Vengeance_Sword = False
 
+
 def tprint(s):
     print(s)
     time.sleep(2)
 
+
 def game_over():
+    global totalscore
+    totalscore = -5000
     print(". . . GAME OVER . . .")
     time.sleep(3)
-    sys.exit()
+
 
 def fight():
-    monsters = ['Omega dragon', 'weakminion', 'groupofweakminion', 'big zombie','rock']
+    global totalscore
+    tprint("Your total score is " + str(totalscore))
+    monsters = ['Omega dragon', 'weakminion']
     whichmonster = random.choice(monsters)
-    if whichmonster=="Omega dragon":
+    if whichmonster == "Omega dragon":
         totalscore += omega_dragon()
-    if whichmonster=="weakminion":
+    elif whichmonster == "weakminion":
         totalscore += weakminion()
-    if whichmonster=="rock":
-        totalscore += rock()
-        
+
+
 def omega_dragon():
     tprint("Peto: OH NO IT IS THE OMEGA DRAGON WHAT SHOULD WE DO?!")
     print("* What will you do? *")
@@ -33,54 +37,76 @@ def omega_dragon():
     print("2 - Attack the monster")
     print("3 - Defend against the monster")
     choice = input()
-    if choice == "1":
-        tprint("Peto: QUICK IMA THROW SOME ROCKS TO DISTRACT HIM!!")
-        tprint("* you both run away from the dragon *")
-        return 0
-    elif choice == "2":
-        if "Omega Katana" in backpack:
-            tprint(name + "Do not fear for the omega katana is here")
-            tprint("The monster is defeated with only one blow with the omega katana good job you earn 200 points")
-            return 200
+    while True:
+        if choice == "1":
+            tprint("Peto: QUICK IMA THROW SOME ROCKS TO DISTRACT HIM!!")
+            tprint("* you both run away from the dragon and lose 10 points *")
+            return -10
+        elif choice == "2":
+            if "Omega Katana" in backpack:
+                tprint(name + ": Do not fear for the Omega Katana is here")
+                tprint("The monster is defeated with only one blow with the Omega Katana. Good job, you earn 200 points.")
+                return 200
+            else:
+                tprint("* you strike the monster, but he is too strong . . . *")
+                tprint("Peto: I am pretty sure we lost")
+                tprint("* the monster defeats you with its fire breath *")
+                return -1000
+        elif choice == "3":
+            tprint("Peto: I will defend myself")
+            tprint("* you both defend against the monster *")
+            tprint("Peto: I am pretty sure we are dead the monster is too strong")
+            return -1000
         else:
-            tprint("* you strike the monster, but he is too strong . . . *")
-            tprint("Peto: i am pretty sure we lost")
-            tprint("* the monster defeats you with its fire breath *")
-            game_over()
-    else:
-        print ("Peto: I do not think that is a choice")
+            print("Peto: I do not think that is a choice")
+            continue
+
 
 def weakminion():
-    tprint("* a small litte cute creature comes out from under a rock *")
-    choice = input("1 - say hi\n2- run away\n3 - kill it\n4 - look at it soo much untill you die of its cuteness!")
+    global Vengeance_Sword
+    tprint("* a small little cute creature comes out from under a rock *")
+    choice = input("1 - say hi\n2 - run away\n3 - kill it\n4 - look at it so much until you die of its cuteness!\n")
     if choice == '1':
-        minionstate = random.randint(1,4)
+        minionstate = random.randint(1, 4)
         if minionstate == 3 and not Vengeance_Sword:
             tprint(name + ": hi there little guy...")
             tprint("Peto: he may be a little shy")
-            tprint("* smiles cutely and gives you the sword of vengeance")
-            backpack.append(["Sword Of Vengeance", "Special powers: can kill an asteroid"])
+            tprint("* smiles cutely and gives you the Sword of Vengeance *")
+            backpack.append("Sword Of Vengeance")
             Vengeance_Sword = True
             return 100
         else:
             tprint("* this little thing grows ten times its size and is ready to eat lunch *")
             tprint(name + "& Peto: AHHHHHHHHHHHHHHHHHHH!!!!!!!!!!!")
-            tprint("* you both run very quickly out of there *")
-            return 0
+            tprint("* you both run very quickly out of there and you lose 10 points *")
+            return -10
+    elif choice == '2':
+        tprint("* you both run away safely and lose 50 points *")
+        return -50
+    elif choice == '3':
+        tprint("* you easily defeat the weak minion *")
+        return 50
+    elif choice == '4':
+        tprint("* you die of its cuteness *")
+        return -1000
+    else:
+        print("Peto: I do not think that is a choice")
+        return 0
+
 
 def rock():
     tprint("* you found a rock *")
-    backpack.append(["rock", "Special powers: can be thrown maybe"])
+    backpack.append("rock")
     return 0
-l = 0
-while l == 0:
 
+
+while True:
     name = input("Please enter your name: ")
     tprint("Oh, that's a beautiful name..")
 
     tprint("Once upon a time...")
     tprint("Two friends watching the news when suddenly...")
-    print("BREAKING NEWS! SCIENTISTS HAVE DISCOVERED AN ASTEROID AS BIG AS MAYBE A RELATIVE OF YOURS!!!")
+    print("BREAKING NEWS! SCIENTISTS HAVE DISCOVERED AN ASTEROID AS BIG AS A PLANET")
     time.sleep(1)
     print("AND IT'S COMING TOWARDS EARTH AT INCREASING SPEEDS OF VERY FAST SPEEDS FAST!!!!!!")
     tprint("...")
@@ -104,24 +130,45 @@ while l == 0:
             break
         else:
             print("We don't have pizza here, sorry?")
-    
+
     tprint(name + ": Well, you have a point. . .")
-    tprint(name + ": Ok lets go gather some items to be able to fight")
+    tprint(name + ": Ok let's go gather some items to be able to fight")
     while True:
         tprint(name + ": So where should we go first?")
         print("1 - Look inside of your home and try to find something useful")
         print("2 - Go into the dangerous forest where there might be something valuable")
-        choice=input()
+        choice = input()
         if choice == "1":
-            tprint("Peto: Look over here i found your secret OMEGA katana!")
+            tprint("Peto: Look over here I found your secret OMEGA katana!")
             tprint(name + ": oh yeah now we are going to be powerful and ready")
-            backpack.append(["Omega Katana", "Special powers: its a katana - its cool - kills maybe"])
+            backpack.append("Omega Katana")
             break
         elif choice == "2":
-            tprint("Peto: Uh i think we will not find anything here i suppose we can just grab this rock")
+            tprint("Peto: Uh I think we will not find anything here I suppose we can just grab this rock")
             tprint(name + ": Well that is disappointing")
-            backpack.append(["rock","Special powers: can be thrown to distract monsters - its beautiful"])
+            backpack.append("rock")
             break
         else:
-            print ("Peto: I do not think that is a choice")
-    tprint(name + ": Ok lets go fight monsters to level up and get stronger and destroy the asteroid")
+            print("Peto: I do not think that is a choice")
+
+    tprint(name + ": Ok let's go fight monsters to level up and get stronger and destroy the asteroid")
+    tprint("* you need a total score of 300 points to win this game and if you get less than -49 points you lose *")
+
+    while totalscore < 300 and totalscore > -50:
+        fight()
+
+    if totalscore <= -50:
+        game_over()
+    elif totalscore >= 300:
+        tprint("Peto: WE DESTROYED THE ASTEROID AND WE WON YAY")
+
+    tprint("Do you want to play again?")
+    print("1 - Yes")
+    print("2 - No")
+    choice = input()
+    if choice == "1":
+        totalscore = 0
+        backpack.clear()
+        Vengeance_Sword = False
+    else:
+        sys.exit()
